4a274a37d7cc0e4141bab2636bd2c9a38cb70c4c
JiHwan Park <bat522@naver.com>
2024-02-05 23:48:21 +0900

    Add new file:   Algorithm_JAVA.iml

---
+++ 문제풀이_BAEKJOON/bj_1935.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class bj_1935 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String expression = br.readLine();
+        String[] suffix = new String[expression.length()];
+
+        for (int i = 0; i < expression.length(); i++) {
+            suffix[i] = String.valueOf(expression.charAt(i));
+        }
+
+        for (String s : suffix) {
+            
+        }
+        
+    }
+}
